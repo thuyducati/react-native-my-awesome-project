dccf331bcf636a73163dcbee10efe307de2642b4
thuylt <thuylt@runsystem.net>
2020-04-21 07:16:24 +0700

    Create splash screen

--- App.js
+++ App.js
@@ -1,114 +1,86 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
+ * Sample React Native App - Splash Screen
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class Splash extends Component {
+  render() {
+    const viewStyles = [
+      styles.container,
+      {
+        backgroundColor: "orange"
+      }
+    ];
+    const textStyles = {
+      color: "#FFFFFF",
+      fontSize: 25,
+      fontWeight: "bold",
+      textAlign: "center"
+    };
+    return (
+      <View style={viewStyles}>
+        <StatusBar hidden={true} />
+        <Text style={textStyles}>
+          Welcome to {"\n"} GMO-Z.com RUNSYSTEM
+        </Text>
+      </View>
+    );
+  };
+}
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+  performTimeConsumingTask = () => {
+    return new Promise((resolve) => {
+      setTimeout(
+        () => resolve("result"),
+        2000
+      )
+    });
+  }
+  async componentDidMount() {
+    // Pre-load data from an external API.
+    // Pre-load data using AsyncStorage.
+    const data = await this.performTimeConsumingTask();
+    if (data != null) {
+      this.setState({
+        isLoading: false
+      });
+    }
+  }
+  render() {
+    if (this.state.isLoading) {
+      return <Splash />;
+    }
+    return (
+      <View style={styles.container} >
+        <Text>
+          Welcome to React Native!!! ^_^
+        </Text>
+        <Text>
+          Reload the App to see a Splash Screen...
+        </Text>
+      </View>
+    );
+  };
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
-
-export default App;

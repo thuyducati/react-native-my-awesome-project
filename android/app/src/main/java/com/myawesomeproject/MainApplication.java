2e583a292382c731e065432600b09b5d0790a2f5
thuylt <thuylt@runsystem.net>
2020-04-28 21:27:51 +0700

    Successfully installed and connected the React Native Android App with Firebase

--- android/app/src/main/java/com/myawesomeproject/MainApplication.java
+++ android/app/src/main/java/com/myawesomeproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.myawesomeproject;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new VectorIconsPackage(),
             new RNCMaskedViewPackage(),
             new SafeAreaContextPackage(),
